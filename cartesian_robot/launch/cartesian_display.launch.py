33efcd90306fa3685053eb180b43e476bd4aecab
VSP-AR <aswathy.ramachandran.menon@ipa.fraunhofer.de>
2025-04-29 22:38:53 +0200

    Updated the launch file

--- cartesian_robot/launch/cartesian_display.launch.py
+++ cartesian_robot/launch/cartesian_display.launch.py
@@ -1,36 +1,26 @@
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, TimerAction
-from launch.substitutions import LaunchConfiguration, Command, PathJoinSubstitution
+from launch.actions import TimerAction
+from launch.substitutions import Command, PathJoinSubstitution
 from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
 
 def generate_launch_description():
-    link_length_arg = DeclareLaunchArgument(
-        name='link_length',
-        default_value='1.0',
-        description='Length of the robot links'
-    )
-
     urdf_path = PathJoinSubstitution([
         FindPackageShare('cartesian_robot'),
         'urdf',
         'cartesian_urdf.xacro'
     ])
-
     robot_description = Command([
-        'xacro', ' ', urdf_path, ' link_length:=', LaunchConfiguration('link_length')
+        'xacro', ' ', urdf_path
     ])
 
     return LaunchDescription([
-        link_length_arg,
-
-        
         Node(
             package='joint_state_publisher_gui',
             executable='joint_state_publisher_gui',
             name='joint_state_publisher_gui',
             output='screen'
-        ),       
+        ),
         Node(
             package='robot_state_publisher',
             executable='robot_state_publisher',
